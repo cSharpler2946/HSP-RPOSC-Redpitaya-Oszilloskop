2aee9379551b4aae6cf7d186f0e7b81838c9e494
sebastian.ederer <sebi.ederer@gmx.de>
2020-12-20 21:12:35 +0100

    Worked on the WebUI

---
+++ LogicAnalyzer.WebUI/js/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+
+    @{{ user.username }} - {{ fullName }}
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'App',
+    data(){
+        return{
+            // Return the data that is used in the component
+            followers: 0,
+            user: {
+                id: 1,
+                username: '_MitchelRomney',
+                fistName: 'Mitchell',
+                lastName: 'Romney',
+                email: 'michellromney@theearthissquare.com',
+                isAdmin: true
+            }
+        }
+    },
+    computed:{
+        fullName(){
+            return `${this.user.firstNam} ${this.user.lastName}`;
+        }
+    }
+}
+</script>
