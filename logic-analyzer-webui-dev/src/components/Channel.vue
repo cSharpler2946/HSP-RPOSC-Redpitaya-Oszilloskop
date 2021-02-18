1b9003e25e8c92aae6c68a4ebef368c80ba3d53b
sebastian.ederer <sebi.ederer@gmx.de>
2021-02-18 21:38:56 +0100

    Started to implement functionality for the channel-settings.

--- logic-analyzer-webui-dev/src/components/Channel.vue
+++ logic-analyzer-webui-dev/src/components/Channel.vue
@@ -18,16 +18,52 @@
             </div>
 
             <div class="col-3 channel-settings">
-                <i class="fas fa-cog channel-settings-btn float-end"></i>
+                <i class="fas fa-cog channel-settings-btn float-end" data-bs-toggle="modal" v-bind:data-bs-target="'#channel-settings-modal-' + channelId"></i>
             </div>
             </div>
         </div>
-        
 
         <div class="signal-box col-md-9 col-12">
 
         </div>
     </div>
+
+<!-- 
+    This modal should be generated each time you click on the correspondig button.
+    If you hard-code this, each time you reopen the settings, the selected element will be the one selected last.
+    However, the last selected element is not equal to the last applied element (the value of the decodeChannel data object).
+ -->
+<div class="modal fade" v-bind:id="'channel-settings-modal-' + channelId" tabindex="-1" aria-labelledby="channelSettings" aria-hidden="true"> 
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Settings for: <b>{{channelName}}</b></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <div class="modal-body">
+        <div class="row">
+            <label class="col-sm-4 col-form-label">Decoder Channel:</label>
+            <div class="col-sm-8">
+                <select v-model="decoderChannel" class="form-select" aria-label="Select decoder channel">
+                    <option selected>none</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </div>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+        <button @click="editChannelSettings(decoderChannel)" type="button" class="btn btn-success" data-bs-dismiss="modal">Ok</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -37,6 +73,7 @@ export default {
         return {
             editedChannel: null,
             maxCharacters: 20,
+            decoderChannel: null,
         }
     },
     props: {
@@ -53,6 +90,10 @@ export default {
     methods: {
         editChannelName: function(channel){
             this.editedChannel = channel;
+        },
+        editChannelSettings: function(decoderChannel){
+            // The parameter decoderChannel contains the value of the selected element.
+            console.log(decoderChannel);
         },
     }
 }
