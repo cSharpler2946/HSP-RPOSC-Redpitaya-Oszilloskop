4ac40209117591c6e1c7dffbfd5efe857d88a9b1
Florian Henneke <Florian.Henneke@rosenberger.com>
2021-02-20 12:33:51 +0100

    next

--- RPOSC-LogicAnalyser/src/main.cpp
+++ RPOSC-LogicAnalyser/src/main.cpp
@@ -14,12 +14,8 @@
 #include <Python.h>
 
 #include "../lib/loguru/loguru.hpp"
-#include "../lib/nlohmann/json.hpp"
 
-#include "Acquirer.hpp"
-#include "SRDDecoderList.hpp"
-#include "ChosenDecoder.hpp"
-#include "SRDRequestedOptions.hpp"
+#include "../lib/nlohmann/json.hpp"
 
 #include "main.h"
 
@@ -30,13 +26,19 @@
 #define SIGNAL_SIZE_DEFAULT      1024
 #define SIGNAL_UPDATE_INTERVAL      10
 
-const char* logfile = "log/debug.log";
-vector<PContainer*> pContainerList;
-vector<SContainer*> sContainerList;
+CFloatSignal ss_signal_1("VOLTAGE", 16384, 0.0f);
+CIntParameter write_pointer_parameter("WRITE_POINTER", CBaseParameter::AccessMode::RW, 0, false, 0, 20000);
+CBooleanParameter start_acquisition("START_ACQUISITION", CBaseParameter::AccessMode::RW, false, false);
 
-static srd_session *srdSession;
-static srd_decoder_inst *srdDecoderInst;
-static Acquirer *activeAcquirer;
+bool acquisition_pending = false;
+bool acquisition_complete = false;
+
+uint32_t buff_size = 16382;
+float buff[16384];
+
+uint32_t previous_write_pointer = 0;
+
+char* logfile = "log/debug.log";
 
 const char *rp_app_desc(void)
 {
@@ -46,8 +48,6 @@ const char *rp_app_desc(void)
 int rp_app_init(void)
 {
     fprintf(stderr, "Loading RPOSC Logic Analyzer\n");
-
-    //Tests
     //loguru::init();
     loguru::add_file(logfile, loguru::Append, loguru::Verbosity_INFO);
     LOG_F(INFO, "THE app staaaarts maaaan....");
@@ -65,7 +65,7 @@ int rp_app_init(void)
     }
 
     //Libsigrokdecode init
-    if ((ToErr srd_init(nullptr)) != SRD_OK)
+    /*if ((ToErr srd_init(nullptr)) != SRD_OK)
     {
         fprintf(stderr, "LibSigrokDecode init failed\n");
         return EXIT_FAILURE;
@@ -73,27 +73,18 @@ int rp_app_init(void)
     else
     {
         fprintf(stderr, "LibSigrokDecode init success: Using version: %s\n", srd_lib_version_string_get());
-    }
-    //End: Tests
+    }*/
 
-    //Set update intveral for signals
+    //Set signal update interval
     CDataManager::GetInstance()->SetSignalInterval(SIGNAL_UPDATE_INTERVAL);
-    //TODO: How about paramters
+    CDataManager::GetInstance()->SetParamInterval(SIGNAL_UPDATE_INTERVAL);
 
-    //Intitialize main app
-    srd_session_new(&srdSession);
-
-    //Initiaize all PContainers and SContainers
-    SRDDecoderList *decoderList = new SRDDecoderList("SRDDecoderList", 256, "");
-    sContainerList.push_back(decoderList);
-    SRDRequestedOptions *reqOptions = new SRDRequestedOptions("SRDRequestedOptions", 127, "", srdDecoderInst);
-    sContainerList.push_back(reqOptions);
-    ChosenDecoder *chosenDecoder = new ChosenDecoder("ChosenDecoder", CBaseParameter::RW, "", false, reqOptions, srdDecoderInst);
-    pContainerList.push_back(chosenDecoder);
-
-
-    //activeAcquirer = new Acquirer(); //TODO: Get parameter (ACQChosenOption)
-
+    rp_AcqReset();
+    usleep(50);
+    rp_AcqSetDecimation(RP_DEC_8192);
+    rp_AcqSetTriggerDelay(8192);
+    rp_AcqSetGain(RP_CH_1, RP_HIGH);
+    rp_AcqStart();
     usleep(100);
 
     return 0;
@@ -126,46 +117,43 @@ int rp_get_signals(float ***s, int *sig_num, int *sig_len)
 }
 /* Internal functions end */
 
-
-
 void UpdateSignals(void){
-    // Call Update UpdateFunctions of all signals.
-    /*
-    for(SContainer curr: sContainerList)
+  if(acquisition_pending && acquisition_complete)
     {
-        curr.Update();
+        acquisition_pending = acquisition_complete = false;
+        rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
+        std::vector<float> v(buff, buff + 16384);
+        ss_signal_1.Set(v);
     }
-    */
 }
 
 void UpdateParams(void){
-    // Call Update functions of all parameters.
-    /*
-    for(PContainer curr: pContainerList)
+  uint32_t write_pointer;
+    rp_AcqGetWritePointer(&write_pointer);
+    write_pointer_parameter.SendValue(write_pointer);
+    if(previous_write_pointer == write_pointer)
     {
-        curr.Update();
+        acquisition_complete = true;
     }
-    */
+    previous_write_pointer = write_pointer;
+
+    if(start_acquisition.IsNewValue())
+    {
+        start_acquisition.Set(false);
+        start_acquisition.Update();
+        rp_AcqSetTriggerSrc(RP_TRIG_SRC_NOW);
+        acquisition_pending = true;
+    }
 }
 
 /**
  * Callback function, which gets called when paramters changed.
  */
-void OnNewParams(void){
-    for(PContainer *curr: pContainerList)
-    {
-        curr->OnNew();
-    }
-}
+void OnNewParams(void){}
 
 /**
  * Callback function, which gets called when signals changed.
  */
-void OnNewSignals(void){
-    for(SContainer *curr: sContainerList)
-    {
-        curr->OnNew();
-    }
-}
+void OnNewSignals(void){}
 
 void PostUpdateSignals(void){}
