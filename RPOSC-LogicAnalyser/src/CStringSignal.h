2c26c16a8183878d789b89bc439a1182bf51dcd8
Florian Henneke <Florian.Henneke@rosenberger.com>
2021-02-20 11:59:44 +0100

    next

--- RPOSC-LogicAnalyser/src/CStringSignal.h
+++ RPOSC-LogicAnalyser/src/CStringSignal.h
@@ -3,16 +3,20 @@
 #include <CustomParameters.h>
 #include <string>
 
-using namespace std;
-
 //TODO: Class has to be tested!
 
-class CStringSignal : public CCustomSignal<string>
+/*
+class CStringSignal : public CCustomSignal<std::string>
 {
 public:
-	CStringSignal(std::string _name, int _size, string _def_value)
+	CStringSignal(std::string _name, int _size, std::string _def_value)
 		:CCustomSignal(_name, _size, _def_value){};
 
-	CStringSignal(std::string _name, CBaseParameter::AccessMode _access_mode, int _size, string _def_value)
+	CStringSignal(std::string _name, CBaseParameter::AccessMode _access_mode, int _size, std::string _def_value)
 		:CCustomSignal(_name, _access_mode, _size, _def_value){};
+};
+*/
+
+class CStringSignal {
+
 };
