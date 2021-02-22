baf74aaaa853d6985013026e127ad38c97a3fcc1
cSharpler2946 <michael.schneider96@web.de>
2021-02-22 21:56:19 +0100

    tried fixing nlohmann

--- RPOSC-LogicAnalyser/src/AnnotationData.hpp
+++ RPOSC-LogicAnalyser/src/AnnotationData.hpp
@@ -8,6 +8,6 @@ class AnnotationData: public SContainer{
     public:
         AnnotationData(std::string _name, int _size, std::string _def_value, srd_decoder_inst *_decoderInst);
         void callbackAnnotation(struct srd_proto_data *pdata, void *cb_data);
-        vector<std::string> annotationData;
+        std::vector<std::string> annotationData;
         srd_decoder_inst *decoderInst;
 };
