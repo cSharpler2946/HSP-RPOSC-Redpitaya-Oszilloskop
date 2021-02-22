a4910d938d7708633da729864132a3530724cc96
Florian Henneke <Florian.Henneke@rosenberger.com>
2021-02-22 11:16:34 +0100

    Added commented out functionality of setting options

--- RPOSC-LogicAnalyser/src/SRDChosenOptions.hpp
+++ RPOSC-LogicAnalyser/src/SRDChosenOptions.hpp
@@ -6,7 +6,7 @@
 class SRDChosenOptions: public SContainer {
     public:
         SRDChosenOptions(std::string _name, int _size, std::string _def_value, srd_decoder_inst *_decoderInst, AllOptionsValid *_allOptionsValid);
-        srd_error_code setOptions(srd_decoder_inst * inst, map<string,string> options);
+        srd_error_code setOption(srd_decoder_inst * inst, map<string,string> options);
         void OnNewInternal();
         bool optionsValid = false;
         srd_decoder_inst *decoderInst;
