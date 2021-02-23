5b050666b414efd6958a6c91b9a943706b2d1900
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 17:16:27 +0100

    push to rp

--- RPOSC-LogicAnalyser/src/LogicSession.hpp
+++ RPOSC-LogicAnalyser/src/LogicSession.hpp
@@ -21,6 +21,8 @@ NLOHMANN_JSON_SERIALIZE_ENUM(MeasurementState, {
 class LogicSession: public PContainer {
     public:
         LogicSession(std::string name, CBaseParameter::AccessMode am, std::string defaultVal, int fpga_update, Acquirer *_acquirer, srd_session *_srdSession, MeasuredData *_measuredData);
+        void Update();
+        void OnNewInternal;
         void runMeasurement();
         MeasurementState measurementState;
         Acquirer *acquirer;
