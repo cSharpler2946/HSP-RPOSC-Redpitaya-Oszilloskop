8da1e016c26a2738e5cb6678643471454486dcd7
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 18:17:44 +0100

    push to rp

--- RPOSC-LogicAnalyser/src/Error.cpp
+++ RPOSC-LogicAnalyser/src/Error.cpp
@@ -2,20 +2,7 @@
 #include "../lib/loguru/loguru.hpp"
 #include <vector>
 
-Error::Error(std::string _name, int _size, std::string _def_value):
-SContainer(_name, _size, _def_value) {
-    LOG_F(INFO, "Loaded Error object");
-    maxSize=_size;
-    loguru::add_callback("network_logger", this->callbackError, nullptr, loguru::Verbosity_INFO);
-    LOG_F(INFO, "Added error callback for verbosity INFO");
-}
-
-void Error::Update() {
-    std::vector<std::string> v{ std::begin(errorList), std::end(errorList) }; //TODO: Check if this works. Reason: Use List to create a buffer, that is never bigger than maxSize, Evtl. create OnNewInternal to sync data with frontend
-    VALUE->Set(v);
-}
-
-void Error::callbackError(void* user_data, const loguru::Message& message) {
+void callbackError(void* user_data, const loguru::Message& message) {
     std::string completeMessage(message.preamble);
     completeMessage.append(message.indentation).append(message.prefix).append(message.message);
     errorList.push_back(completeMessage);
@@ -23,4 +10,17 @@ void Error::callbackError(void* user_data, const loguru::Message& message) {
         errorList.pop_front();
 
     Update();
+}
+
+Error::Error(std::string _name, int _size, std::string _def_value):
+SContainer(_name, _size, _def_value) {
+    LOG_F(INFO, "Loaded Error object");
+    maxSize=_size;
+    loguru::add_callback("network_logger", callbackError, nullptr, loguru::Verbosity_INFO);
+    LOG_F(INFO, "Added error callback for verbosity INFO");
+}
+
+void Error::Update() {
+    std::vector<std::string> v{ std::begin(errorList), std::end(errorList) }; //TODO: Check if this works. Reason: Use List to create a buffer, that is never bigger than maxSize, Evtl. create OnNewInternal to sync data with frontend
+    VALUE->Set(v);
 }
