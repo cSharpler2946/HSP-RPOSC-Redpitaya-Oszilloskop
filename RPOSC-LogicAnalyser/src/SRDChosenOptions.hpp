e5c43044be23179c792374438268b2a2c2ea5829
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 17:32:03 +0100

    push to rp

--- RPOSC-LogicAnalyser/src/SRDChosenOptions.hpp
+++ RPOSC-LogicAnalyser/src/SRDChosenOptions.hpp
@@ -3,11 +3,12 @@
 #include <libsigrokdecode/libsigrokdecode.h>
 #include "AllOptionsValid.hpp"
 #include <map>
+#include <string>
 
 class SRDChosenOptions: public SContainer {
     public:
         SRDChosenOptions(std::string _name, int _size, std::string _def_value, srd_decoder_inst *_decoderInst, AllOptionsValid *_allOptionsValid);
-        srd_error_code setOption(srd_decoder_inst * inst, std::map<string,string> options);
+        srd_error_code setOption(srd_decoder_inst * inst, std::map<std::string,std::string> options);
         void OnNewInternal();
         bool optionsValid = false;
         srd_decoder_inst *decoderInst;
