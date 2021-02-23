69d483d2a6ed61bb90e20b9ad118724bb5e609c0
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 19:51:28 +0100

    next

--- RPOSC-LogicAnalyser/src/main.cpp
+++ RPOSC-LogicAnalyser/src/main.cpp
@@ -79,7 +79,7 @@ int rp_app_init(void)
     sContainerList.push_back(reqOptions);
     SRDChannels *srdChannels = new SRDChannels("SRDChannels", 16, "", srdDecoderInst);
     sContainerList.push_back(srdChannels);
-    AllOptionsValid allOptionsValid = new AllOptionsValid("AllOptionsValid", CBaseParameter::RW, "", false);
+    AllOptionsValid *allOptionsValid = new AllOptionsValid("AllOptionsValid", CBaseParameter::RW, "", false);
     pContainerList.push_back(allOptionsValid);
     ChosenDecoder *chosenDecoder = new ChosenDecoder("ChosenDecoder", CBaseParameter::RW, "", false, reqOptions, srdChannels, srdSession, srdDecoderInst, allOptionsValid);
     pContainerList.push_back(chosenDecoder);
