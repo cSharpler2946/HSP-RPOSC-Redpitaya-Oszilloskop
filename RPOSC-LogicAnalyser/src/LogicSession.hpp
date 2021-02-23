557389adf36eccd9863f5a9c2ae829bcbb7dd443
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 17:18:49 +0100

    push to rp

--- RPOSC-LogicAnalyser/src/LogicSession.hpp
+++ RPOSC-LogicAnalyser/src/LogicSession.hpp
@@ -22,7 +22,7 @@ class LogicSession: public PContainer {
     public:
         LogicSession(std::string name, CBaseParameter::AccessMode am, std::string defaultVal, int fpga_update, Acquirer *_acquirer, srd_session *_srdSession, MeasuredData *_measuredData);
         void Update();
-        void OnNewInternal;
+        void OnNewInternal();
         void runMeasurement();
         MeasurementState measurementState;
         Acquirer *acquirer;
