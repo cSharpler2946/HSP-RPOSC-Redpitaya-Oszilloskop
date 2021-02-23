f92b74b5f27707fedfb277c3225ceaa4d2571b5a
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 18:11:15 +0100

    push to rp

--- RPOSC-LogicAnalyser/src/Error.cpp
+++ RPOSC-LogicAnalyser/src/Error.cpp
@@ -6,7 +6,7 @@ Error::Error(std::string _name, int _size, std::string _def_value):
 SContainer(_name, _size, _def_value) {
     LOG_F(INFO, "Loaded Error object");
     maxSize=_size;
-    loguru::add_callback("network_logger", this.callbackError, nullptr, loguru::Verbosity_INFO);
+    loguru::add_callback("network_logger", this->callbackError, nullptr, loguru::Verbosity_INFO);
     LOG_F(INFO, "Added error callback for verbosity INFO");
 }
 
@@ -19,7 +19,7 @@ void Error::callbackError(void* user_data, const loguru::Message& message) {
     std::string completeMessage(message.preamble);
     completeMessage.append(message.indentation).append(message.prefix).append(message.message);
     errorList.push_back(completeMessage);
-    if(errorList.size > maxSize)
+    if(errorList.size() > maxSize)
         errorList.pop_front();
 
     Update();
