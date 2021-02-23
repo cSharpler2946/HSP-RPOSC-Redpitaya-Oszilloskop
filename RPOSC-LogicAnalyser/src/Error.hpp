a7fc7a92372e74deaf0a11afbe9d91ab1c65e5b1
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-23 17:48:11 +0100

    push to rp

--- RPOSC-LogicAnalyser/src/Error.hpp
+++ RPOSC-LogicAnalyser/src/Error.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "SContainer.hpp"
+#include "../lib/loguru/loguru.hpp"
 #include <string>
 #include <list>
 
 class Error: public SContainer {
     public:
         Error(std::string _name, int _size, std::string _def_value);
+        void Update;
         void callbackError(void* user_data, const loguru::Message& message);
         int maxSize;
-        std::List<string> errorList;
-}
+        std::List<std::string> errorList;
+};
