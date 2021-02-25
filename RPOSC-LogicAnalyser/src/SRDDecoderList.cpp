d1bf2b95edcf6d202c8fb3219a7a7b9d19b3a23d
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-25 16:00:16 +0100

    CHanged ' to \"

--- RPOSC-LogicAnalyser/src/SRDDecoderList.cpp
+++ RPOSC-LogicAnalyser/src/SRDDecoderList.cpp
@@ -15,8 +15,8 @@ void SRDDecoderList::Update() {
     */
     LOG_F(INFO, "Creating and sending decoder list");
     vector<string> decoders;
-    decoders.push_back("{'id': 'I2C','name': 'I2C','longname': 'I squared C','desc': 'Synchronous open drain protocol with one controller and many peripherals.'}");
-    decoders.push_back("{'id': 'UART','name': 'UART','longname': 'UART RS232','desc': 'Asynchronous point-to-point protocol. Very old.'}");
+    decoders.push_back("{\"id\": \"I2C\",\"name\": \"I2C\",\"longname\": \"I squared C\",\"desc\": \"Synchronous open drain protocol with one controller and many peripherals.\"}");
+    decoders.push_back("{\"id\": \"UART\",\"name\": \"UART\",\"longname\": \"UART RS232\",\"desc\": \"Asynchronous point-to-point protocol. Very old.\"}");
 
     //Update VALUE
     VALUE->Set(decoders);
