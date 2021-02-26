fae7f8f06d56c69a04af2732c1bbd9149a42f45f
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-26 17:25:38 +0100

    next

--- RPOSC-LogicAnalyser/src/SPContainer.hpp
+++ RPOSC-LogicAnalyser/src/SPContainer.hpp
@@ -8,6 +8,6 @@ class SPContainer {
         //virtual std::string toJsonString()=0;
         //virtual void fromJsonString(std::string)=0;
         virtual void OnNew()=0;
-        virtual void OnNewInternal(){LOG_IF(INFO, "In SPContainer::OnNewInternal -- Unimplemented receive funtion!!")};
+        virtual void OnNewInternal(){LOG_F(INFO, "In SPContainer::OnNewInternal -- Unimplemented receive funtion!!")};
         virtual void Update(){};
 };
