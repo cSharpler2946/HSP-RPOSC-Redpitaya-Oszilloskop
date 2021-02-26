298d1a4be0d69e9036af6b7f026b9674f530ef1b
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-02-26 15:31:41 +0100

    next

--- RPOSC-LogicAnalyser/src/main.cpp
+++ RPOSC-LogicAnalyser/src/main.cpp
@@ -130,12 +130,12 @@ void OnNewParams(void){
     if(startup->IsValueChanged())
     {
         LOG_F(INFO, "Creating and sending decoder list");
-        vector<string> decoders;
+        std::vector<string> decoders;
         decoders.push_back("{\"id\": \"I2C\",\"name\": \"I2C\",\"longname\": \"I squared C\",\"desc\": \"Synchronous open drain protocol with one controller and many peripherals.\"}");
         decoders.push_back("{\"id\": \"UART\",\"name\": \"UART\",\"longname\": \"UART RS232\",\"desc\": \"Asynchronous point-to-point protocol. Very old.\"}");
 
         //Update VALUE
-        VALUE->Set(decoders);
+        decoderList->Set(decoders);
 
         startup->Update();
     }
