2829f13708e946736b70ae75f24db117b88e0122
AlexLeoSchmid <alexander.l.m.schmid@gmail.com>
2021-03-20 20:38:59 +0100

    Typescript can now be used

--- logic-analyzer-webui-dev/src/components/DecodedData.vue
+++ logic-analyzer-webui-dev/src/components/DecodedData.vue
@@ -2,14 +2,14 @@
     <div class="card">
         <div class="card-body">
             <p class="card-text decoded-data">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
-                aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo 
-                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus 
-                est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur 
-                sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore 
-                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo 
-                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est 
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
                 Lorem ipsum dolor sit amet.
             </p>
         </div>
@@ -18,10 +18,10 @@
 
 <script>
 export default {
-    name: "DecodedData",
-    props: {
+  name: 'DecodedData',
+  props: {
 
-    }
+  }
 }
 </script>
 
--- logic-analyzer-webui-dev/src/main.ts
+++ logic-analyzer-webui-dev/src/main.ts
@@ -10,10 +10,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlay, faCaretDown, faCog } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// Add the fontawesome icons you want to use to the library. 
+// Add the fontawesome icons you want to use to the library.
 // By doing so, only the used icons will get used in the productive version.
 library.add(faPlay, faCaretDown, faCog)
 
-const app = createApp(App);
+const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.mount('#app');
+app.mount('#app')
