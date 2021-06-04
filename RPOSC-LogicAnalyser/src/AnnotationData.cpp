489e6786f2698bbc2c45c7f247786fe004ed9f0c
florianhenneke <florian.henneke@st.oth-regensburg.de>
2021-06-04 11:56:12 +0200

    Added untested version of annotaionData

--- RPOSC-LogicAnalyser/src/AnnotationData.cpp
+++ RPOSC-LogicAnalyser/src/AnnotationData.cpp
@@ -2,13 +2,17 @@
 #include "../lib/nlohmann/jsonWrapper.hpp"
 #include <loguru.hpp>
 
-AnnotationData::AnnotationData(std::string _name, int _size, std::string _def_value, srd_decoder_inst *_decoderInst):
+AnnotationData::AnnotationData(std::string _name, int _size, std::string _def_value, srd_session *_srdSession):
 SContainer(_name, _size, _def_value) {
     LOG_F(INFO, "Created annotation data");
-    decoderInst = _decoderInst;
+    srdSession = _srdSession;
 
     //TODO: register callback for annotations
-    // ToErr srd_pd_output_callback_add(sess, SRD_OUTPUT_ANN, &callbackAnnotation, nullptr);
+    srd_error_code ret = SRD_OK;
+    if((ret = ToErr srd_pd_output_callback_add(srdSession, SRD_OUTPUT_ANN, &callbackAnnotation, this)) != SRD_OK)
+    {
+        LOG_F(ERROR, "Couldn't send data to srd session: Error code : %d", ret);
+    }
 }
 
 void AnnotationData::Update() {
@@ -16,7 +20,7 @@ void AnnotationData::Update() {
 }
 
 void AnnotationData::callbackAnnotation(struct srd_proto_data *pdata, void *cb_data) {
-    /* TODO: Implement function following the old lsrd 0.3.0 api version...
+    /* //TODO: Implement function following the old lsrd 0.3.0 api version...
     srd_proto_data_annotation *data = (srd_proto_data_annotation *)pdata->data;
     char **annString = (gchar **)g_slist_nth_data(pdata->pdo->di->decoder->annotations, data->ann_format); //double pointer!!, ann_format is called ann_class in lsrd4
     LOG_F(INFO, "Got annotation data: %d-%d: %d: %s: %s\n", pdata->start_sample, pdata->end_sample, data->ann_format, *annString, *data->ann_text);
@@ -27,8 +31,8 @@ void AnnotationData::callbackAnnotation(struct srd_proto_data *pdata, void *cb_d
     tmp["annotationClass"]=*annString;
     tmp["annotationText"]=*data->ann_text; //TODO: Test if these dereferences are right
 
-    annotationData.push_back(tmp.dump());
-    Update();
+    ((AnnotationData *)cb_data)->annotationData.push_back(tmp.dump());
+    ((AnnotationData *)cb_data)->Update();
     */
 }
 
--- RPOSC-LogicAnalyser/src/AnnotationData.hpp
+++ RPOSC-LogicAnalyser/src/AnnotationData.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#define ToErr (srd_error_code)
 
 #include "SContainer.hpp"
 #include <libsigrokdecode/libsigrokdecode.h>
@@ -7,10 +8,10 @@
 
 class AnnotationData: public SContainer{
     public:
-        AnnotationData(std::string _name, int _size, std::string _def_value, srd_decoder_inst *_decoderInst);
-        void callbackAnnotation(struct srd_proto_data *pdata, void *cb_data);
+        AnnotationData(std::string _name, int _size, std::string _def_value, srd_session *_srdSession);
+        static void callbackAnnotation(struct srd_proto_data *pdata, void *cb_data);
         void Update();
         void resetData();
         std::vector<std::string> annotationData;
-        srd_decoder_inst *decoderInst;
+        srd_session *srdSession;
 };
