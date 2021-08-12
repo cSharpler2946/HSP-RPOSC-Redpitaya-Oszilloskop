31b1a2243120a49cc454af2de3e9cec11150ae49
cSharpler2946 <michael.schneider96@web.de>
2021-08-12 18:37:35 +0200

    some good stuff

--- RPOSC-LogicAnalyser/src/AcquirerConstants.hpp
+++ RPOSC-LogicAnalyser/src/AcquirerConstants.hpp
@@ -11,7 +11,7 @@ class AcquirerConstants
     static vector<double> supportedSampleRates;
     static vector<string> availableChannels;
     static vector<int> supportedDecimations;
-    static vector<string> supportedAttenuation;
+    static vector<int> supportedAttenuation;
     static vector<string> supportedPinState;
     static vector<int> supportedPinStateInt;
     static int maxSamples;
