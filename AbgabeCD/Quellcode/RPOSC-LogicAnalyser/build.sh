ca5d42fe1d04bd34712aef82461f53894be450a0
Alexander Schmid <alexander.l.m.schmid@gmail.com>
2021-09-23 14:58:29 +0200

    Removed dependencies from AbgabeCD

--- AbgabeCD/Quellcode/RPOSC-LogicAnalyser/build.sh
+++ AbgabeCD/Quellcode/RPOSC-LogicAnalyser/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -d "${root_dir}/externalRedpitaya" ] && [ ! -d "${root_dir}/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf" ]; then
+    echo "Missing dependencies: Due to file size constraints, not all necessary dependencies can be included."
+    echo "Please clone the following repository and use that as your starting point for the project: https://github.com/cSharpler2946/HSP-RPOSC-Redpitaya-Oszilloskop."
+    exit 1
+fi
+
 root_dir=$(dirname $0)
 echo "Root directory:" $root_dir
 
--- AbgabeCD/Quellcode/RPOSC-LogicAnalyser/cleanAndBuild.sh
+++ AbgabeCD/Quellcode/RPOSC-LogicAnalyser/cleanAndBuild.sh
@@ -3,6 +3,12 @@
 root_dir=$(dirname $0)
 echo "Root directory:" $root_dir
 
+if [ ! -d "${root_dir}/externalRedpitaya" ] && [ ! -d "${root_dir}/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf" ]; then
+    echo "Missing dependencies: Due to file size constraints, not all necessary dependencies can be included."
+    echo "Please clone the following repository and use that as your starting point for the project: https://github.com/cSharpler2946/HSP-RPOSC-Redpitaya-Oszilloskop."
+    exit 1
+fi
+
 echo "Removing old build dir: " $root_dir/build
 rm -rf $root_dir/build
 
